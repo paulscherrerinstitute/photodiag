c4749cd38cc580f62ba6507659f26e5acfec3497
Ivan Usov <ivan.usov@psi.ch>
2018-05-28 14:42:23 +0200

    README and setup.py cleanup

--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 The Photon Arrival and Length Monitor (PALM) is a device at SwissFEL for temporal diagnostics of the photon beam. It measures the arrival time of an FEL pulses relative to the pump laser pulses in the experimental hutches, as well as the FEL pulse lengths.
 
 This repository contains code for data analysis of waveforms acquired by the PALM setup.
-
-## How to Run It
-
-
-## Key Dependencies
--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='photdiag',
-    version='0.1.0',
-    author='Ivan Usov',
-    author_email='ivan.usov@psi.ch',
-    description='Photon diagnostics tools at SwissFel',
-    license='GNU GPLv3',
-    url='',
-    packages=['photdiag'],
-    install_requires=['h5py', 'numpy', 'scipy'],
+    name="photdiag",
+    version="0.1.0",
+    author="Ivan Usov",
+    author_email="ivan.usov@psi.ch",
+    description="Photon diagnostics tools at SwissFel",
+    packages=["photdiag"],
+    license="GNU GPLv3",
 )
