8ba1e7028a961e37e6cb70c2e568e1005ca345a1
Ivan Usov <ivan.usov@psi.ch>
2018-08-13 15:41:01 +0200

    add 'beamline' argument to cli

--- apps/palm/receiver.py
+++ apps/palm/receiver.py
@@ -1,18 +1,30 @@
+import argparse
 from collections import deque
+
 from bsread import source
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('beamline')
+args = parser.parse_args()
+
+if args.beamline == 'alvra':
+    reference = 'SAROP11-PALMK118:CH1_BUFFER'
+    streaked = 'SAROP11-PALMK118:CH2_BUFFER'
+
+elif args.beamline == 'bernina':
+    reference = 'SAROP21-PALMK134:CH1_BUFFER'
+    streaked = 'SAROP21-PALMK134:CH2_BUFFER'
+
+else:
+    raise Exception(f"{args.beamline} - unknown beamline")
+
 
 BUFFER_SIZE = 100
 data_buffer = deque(maxlen=BUFFER_SIZE)
 
 state = 'polling'
 
-# bernina
-# reference = 'SAROP21-PALMK134:CH1_BUFFER'
-# streaked = 'SAROP21-PALMK134:CH2_BUFFER'
-
-# alvra
-reference = 'SAROP11-PALMK118:CH1_BUFFER'
-streaked = 'SAROP11-PALMK118:CH2_BUFFER'
 
 def stream_receive():
     global state
