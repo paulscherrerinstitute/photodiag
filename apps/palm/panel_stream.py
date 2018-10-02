53e877021b33981dd41876072bfecc2cd6209e12
Ivan Usov <ivan.usov@psi.ch>
2018-10-02 17:57:01 +0200

    Panel stream code refactoring

--- apps/palm/panel_stream.py
+++ apps/palm/panel_stream.py
@@ -49,15 +49,15 @@ def create(palm):
 
     # ---- line glyphs
     waveform_source = ColumnDataSource(dict(x_str=[], y_str=[], x_ref=[], y_ref=[]))
-    reference_l = waveform_plot.add_glyph(
+    waveform_ref_line = waveform_plot.add_glyph(
         waveform_source, Line(x='x_ref', y='y_ref', line_color='blue'))
-    streaked_l = waveform_plot.add_glyph(
+    waveform_str_line = waveform_plot.add_glyph(
         waveform_source, Line(x='x_str', y='y_str', line_color='red'))
 
     # ---- legend
     waveform_plot.add_layout(Legend(items=[
-        ("reference", [reference_l]),
-        ("streaked", [streaked_l])
+        ("reference", [waveform_ref_line]),
+        ("streaked", [waveform_str_line])
     ]))
     waveform_plot.legend.click_policy = "hide"
 
@@ -89,18 +89,17 @@ def create(palm):
 
     # ---- line glyphs
     xcorr_source = ColumnDataSource(dict(lags=[], xcorr1=[], xcorr2=[]))
-    xcorr_pos_source = ColumnDataSource(dict(pos=[]))
     xcorr_plot.add_glyph(
         xcorr_source, Line(x='lags', y='xcorr1', line_color='purple', line_dash='dashed'))
     xcorr_plot.add_glyph(xcorr_source, Line(x='lags', y='xcorr2', line_color='purple'))
 
     # ---- vertical span
-    xcorr_plot_pos = Span(location=0, dimension='height')
-    xcorr_plot.add_layout(xcorr_plot_pos)
+    xcorr_center_span = Span(location=0, dimension='height')
+    xcorr_plot.add_layout(xcorr_center_span)
 
 
     # Delays plot
-    delay_plot = Plot(
+    pulse_delay_plot = Plot(
         title=Title(text="Pulse delays"),
         x_range=DataRange1d(),
         y_range=DataRange1d(),
@@ -111,31 +110,31 @@ def create(palm):
     )
 
     # ---- tools
-    delay_plot.add_tools(PanTool(), WheelZoomTool(), ResetTool())
+    pulse_delay_plot.add_tools(PanTool(), WheelZoomTool(), ResetTool())
 
     # ---- axes
-    delay_plot.add_layout(
+    pulse_delay_plot.add_layout(
         LinearAxis(axis_label='Pulse number'), place='below')
-    delay_plot.add_layout(
+    pulse_delay_plot.add_layout(
         LinearAxis(axis_label='Pulse delay (uncalib), eV', major_label_orientation='vertical'),
         place='left')
 
     # ---- grid lines
-    delay_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
-    delay_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
+    pulse_delay_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
+    pulse_delay_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
 
     # ---- line glyphs
-    delay_source = ColumnDataSource(dict(pulse=[], delay=[]))
-    pulse_pos_source = ColumnDataSource(dict(pos=[]))
-    delay_plot.add_glyph(delay_source, Line(x='pulse', y='delay', line_color='steelblue'))
+    pulse_delay_source = ColumnDataSource(dict(pulse=[], delay=[]))
+    pulse_delay_plot.add_glyph(
+        pulse_delay_source, Line(x='pulse', y='delay', line_color='steelblue'))
 
     # ---- vertical span
-    delay_plot_pos = Span(location=0, dimension='height')
-    delay_plot.add_layout(delay_plot_pos)
+    pulse_delay_plot_span = Span(location=0, dimension='height')
+    pulse_delay_plot.add_layout(pulse_delay_plot_span)
 
 
     # Pulse lengths plot
-    pulse_len_plot = Plot(
+    pulse_length_plot = Plot(
         title=Title(text="Pulse lengths"),
         x_range=DataRange1d(),
         y_range=DataRange1d(),
@@ -146,26 +145,26 @@ def create(palm):
     )
 
     # ---- tools
-    pulse_len_plot.add_tools(PanTool(), WheelZoomTool(), ResetTool())
+    pulse_length_plot.add_tools(PanTool(), WheelZoomTool(), ResetTool())
 
     # ---- axes
-    pulse_len_plot.add_layout(
+    pulse_length_plot.add_layout(
         LinearAxis(axis_label='Pulse number'), place='below')
-    pulse_len_plot.add_layout(
+    pulse_length_plot.add_layout(
         LinearAxis(axis_label='Pulse length (uncalib), eV', major_label_orientation='vertical'),
         place='left')
 
     # ---- grid lines
-    pulse_len_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
-    pulse_len_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
+    pulse_length_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
+    pulse_length_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
 
     # ---- line glyphs
-    pulse_len_source = ColumnDataSource(dict(x=[], y=[]))
-    pulse_len_plot.add_glyph(pulse_len_source, Line(x='x', y='y', line_color='steelblue'))
+    pulse_length_source = ColumnDataSource(dict(x=[], y=[]))
+    pulse_length_plot.add_glyph(pulse_length_source, Line(x='x', y='y', line_color='steelblue'))
 
     # ---- vertical span
-    pulse_len_plot_pos = Span(location=0, dimension='height')
-    pulse_len_plot.add_layout(pulse_len_plot_pos)
+    pulse_length_plot_span = Span(location=0, dimension='height')
+    pulse_length_plot.add_layout(pulse_length_plot_span)
 
 
     # Image buffer slider
@@ -185,33 +184,32 @@ def create(palm):
 
 
     # Connect toggle button
-    def stream_button_callback(state):
+    def connect_toggle_callback(state):
         nonlocal connected
         if state:
             connected = True
-            stream_button.label = 'Connecting'
-            stream_button.button_type = 'default'
+            connect_toggle.label = 'Connecting'
+            connect_toggle.button_type = 'default'
 
         else:
             connected = False
-            stream_button.label = 'Connect'
-            stream_button.button_type = 'default'
+            connect_toggle.label = 'Connect'
+            connect_toggle.button_type = 'default'
 
-
-    stream_button = Toggle(label="Connect", button_type='default')
-    stream_button.on_click(stream_button_callback)
+    connect_toggle = Toggle(label="Connect", button_type='default')
+    connect_toggle.on_click(connect_toggle_callback)
 
 
     # Intensity stream reset button
-    def intensity_stream_reset_button_callback():
+    def reset_button_callback():
         nonlocal stream_t
         stream_t = 1  # keep the latest point in order to prevent full axis reset
 
-    intensity_stream_reset_button = Button(label="Reset", button_type='default')
-    intensity_stream_reset_button.on_click(intensity_stream_reset_button_callback)
+    reset_button = Button(label="Reset", button_type='default')
+    reset_button.on_click(reset_button_callback)
 
 
-    # Stream update coroutines
+    # Stream update coroutine
     @gen.coroutine
     def update(message):
         if connected and receiver.state == 'receiving':
@@ -226,6 +224,7 @@ def create(palm):
                 x_ref=palm.energy_range, y_ref=y_ref[0, :])
 
 
+    # Periodic callback to fetch data from receiver
     @gen.coroutine
     def internal_periodic_callback():
         nonlocal current_message
@@ -235,12 +234,12 @@ def create(palm):
 
         if connected:
             if receiver.state == 'polling':
-                stream_button.label = 'Polling'
-                stream_button.button_type = 'warning'
+                connect_toggle.label = 'Polling'
+                connect_toggle.button_type = 'warning'
 
             elif receiver.state == 'receiving':
-                stream_button.label = 'Receiving'
-                stream_button.button_type = 'success'
+                connect_toggle.label = 'Receiving'
+                connect_toggle.button_type = 'success'
 
                 # Set slider to the right-most position
                 if len(receiver.data_buffer) > 1:
@@ -259,7 +258,7 @@ def create(palm):
     tab_stream_layout = column(
         row(
             column(waveform_plot, xcorr_plot), Spacer(width=30),
-            column(buffer_slider, stream_button, intensity_stream_reset_button)),
-        row(delay_plot, Spacer(width=10), pulse_len_plot))
+            column(buffer_slider, connect_toggle, reset_button)),
+        row(pulse_delay_plot, Spacer(width=10), pulse_length_plot))
 
     return Panel(child=tab_stream_layout, title="Stream")
