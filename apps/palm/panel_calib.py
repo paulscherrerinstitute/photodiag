2d383cec25479e16d389b44d2eca06927e6370ba
Ivan Usov <ivan.usov@psi.ch>
2018-10-02 16:45:55 +0200

    Panel calib code refactoring

--- apps/palm/panel_calib.py
+++ apps/palm/panel_calib.py
@@ -12,7 +12,7 @@ from common import WAVEFORM_CANVAS_HEIGHT, WAVEFORM_CANVAS_WIDTH, palm
 doc = curdoc()
 
 # Calibration averaged waveforms per photon energy
-calib_wf_plot = Plot(
+waveform_plot = Plot(
     title=Title(text="eTOF calibration waveforms"),
     x_range=DataRange1d(),
     y_range=DataRange1d(),
@@ -23,49 +23,49 @@ calib_wf_plot = Plot(
 )
 
 # ---- tools
-calib_wf_plot_hover = HoverTool(tooltips=[
+waveform_plot_hover = HoverTool(tooltips=[
     ("energy, eV", '@en'),
     ("etof time, a.u.", '$x'),
     ("intensity, a.u.", '$y'),
 ])
 
-calib_wf_plot.add_tools(PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool(), calib_wf_plot_hover)
+waveform_plot.add_tools(PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool(), waveform_plot_hover)
 
 # ---- axes
-calib_wf_plot.add_layout(
+waveform_plot.add_layout(
     LinearAxis(axis_label='Spectrometer internal time'), place='below')
-calib_wf_plot.add_layout(
+waveform_plot.add_layout(
     LinearAxis(axis_label='Intensity', major_label_orientation='vertical'), place='left')
 
 # ---- grid lines
-calib_wf_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
-calib_wf_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
+waveform_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
+waveform_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
 
 # ---- multiline glyphs
-calib_waveform_source0 = ColumnDataSource(dict(xs=[], ys=[], en=[]))
-reference_ml = calib_wf_plot.add_glyph(
-    calib_waveform_source0, MultiLine(xs='xs', ys='ys', line_color='blue'))
+waveform_ref_source = ColumnDataSource(dict(xs=[], ys=[], en=[]))
+ref_multiline = waveform_plot.add_glyph(
+    waveform_ref_source, MultiLine(xs='xs', ys='ys', line_color='blue'))
 
-calib_waveform_source1 = ColumnDataSource(dict(xs=[], ys=[], en=[]))
-streaked_ml = calib_wf_plot.add_glyph(
-    calib_waveform_source1, MultiLine(xs='xs', ys='ys', line_color='red'))
+waveform_str_source = ColumnDataSource(dict(xs=[], ys=[], en=[]))
+str_multiline = waveform_plot.add_glyph(
+    waveform_str_source, MultiLine(xs='xs', ys='ys', line_color='red'))
 
 # ---- legend
-calib_wf_plot.add_layout(Legend(items=[
-    ("reference", [reference_ml]),
-    ("streaked", [streaked_ml])
+waveform_plot.add_layout(Legend(items=[
+    ("reference", [ref_multiline]),
+    ("streaked", [str_multiline])
 ]))
-calib_wf_plot.legend.click_policy = "hide"
+waveform_plot.legend.click_policy = "hide"
 
 # ---- vertical spans
-phot_peak_pos_ref = Span(location=0, dimension='height', line_dash='dashed', line_color='blue')
-phot_peak_pos_str = Span(location=0, dimension='height', line_dash='dashed', line_color='red')
-calib_wf_plot.add_layout(phot_peak_pos_ref)
-calib_wf_plot.add_layout(phot_peak_pos_str)
+photon_peak_ref_span = Span(location=0, dimension='height', line_dash='dashed', line_color='blue')
+photon_peak_str_span = Span(location=0, dimension='height', line_dash='dashed', line_color='red')
+waveform_plot.add_layout(photon_peak_ref_span)
+waveform_plot.add_layout(photon_peak_str_span)
 
 
 # Calibration fit plot
-calib_fit_plot = Plot(
+fit_plot = Plot(
     title=Title(text="eTOF calibration fit"),
     x_range=DataRange1d(),
     y_range=DataRange1d(),
@@ -76,40 +76,40 @@ calib_fit_plot = Plot(
 )
 
 # ---- tools
-calib_fit_plot.add_tools(PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool())
+fit_plot.add_tools(PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool())
 
 # ---- axes
-calib_fit_plot.add_layout(
+fit_plot.add_layout(
     LinearAxis(axis_label='Photoelectron peak shift'), place='below')
-calib_fit_plot.add_layout(
+fit_plot.add_layout(
     LinearAxis(axis_label='Photon energy, eV', major_label_orientation='vertical'), place='left')
 
 # ---- grid lines
-calib_fit_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
-calib_fit_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
+fit_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
+fit_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
 
 # ---- circle glyphs
-calib_point_source0 = ColumnDataSource(dict(x=[], y=[]))
-reference_c = calib_fit_plot.add_glyph(calib_point_source0, Circle(x='x', y='y', line_color='blue'))
-calib_point_source1 = ColumnDataSource(dict(x=[], y=[]))
-streaked_c = calib_fit_plot.add_glyph(calib_point_source1, Circle(x='x', y='y', line_color='red'))
+fit_ref_circle_source = ColumnDataSource(dict(x=[], y=[]))
+ref_circle = fit_plot.add_glyph(fit_ref_circle_source, Circle(x='x', y='y', line_color='blue'))
+fit_str_circle_source = ColumnDataSource(dict(x=[], y=[]))
+str_circle = fit_plot.add_glyph(fit_str_circle_source, Circle(x='x', y='y', line_color='red'))
 
 # ---- line glyphs
-calib_fit_source0 = ColumnDataSource(dict(x=[], y=[]))
-reference_l = calib_fit_plot.add_glyph(calib_fit_source0, Line(x='x', y='y', line_color='blue'))
-calib_fit_source1 = ColumnDataSource(dict(x=[], y=[]))
-streaked_l = calib_fit_plot.add_glyph(calib_fit_source1, Line(x='x', y='y', line_color='red'))
+fit_ref_line_source = ColumnDataSource(dict(x=[], y=[]))
+ref_line = fit_plot.add_glyph(fit_ref_line_source, Line(x='x', y='y', line_color='blue'))
+fit_str_line_source = ColumnDataSource(dict(x=[], y=[]))
+str_line = fit_plot.add_glyph(fit_str_line_source, Line(x='x', y='y', line_color='red'))
 
 # ---- legend
-calib_fit_plot.add_layout(Legend(items=[
-    ("reference", [reference_c, reference_l]),
-    ("streaked", [streaked_c, streaked_l])
+fit_plot.add_layout(Legend(items=[
+    ("reference", [ref_circle, ref_line]),
+    ("streaked", [str_circle, str_line])
 ]))
-calib_fit_plot.legend.click_policy = "hide"
+fit_plot.legend.click_policy = "hide"
 
 
 # Calibration results datatable
-def calibres_table_source_callback(_attr, _old, new):
+def datatable_source_callback(_attr, _old, new):
     for en, ps0, ps1 in zip(new['energy'], new['peak_pos0'], new['peak_pos1']):
         palm.etofs['0'].calib_data.loc[en, 'calib_tpeak'] = (ps0 if ps0 != 'NaN' else np.nan)
         palm.etofs['1'].calib_data.loc[en, 'calib_tpeak'] = (ps1 if ps1 != 'NaN' else np.nan)
@@ -119,12 +119,12 @@ def calibres_table_source_callback(_attr, _old, new):
         calib_res[etof_key] = palm.etofs[etof_key].fit_calibration_curve()
     update_calibration_plot(calib_res)
 
-calibres_table_source = ColumnDataSource(
+datatable_source = ColumnDataSource(
     dict(energy=['', '', ''], peak_pos0=['', '', ''], peak_pos1=['', '', '']))
-calibres_table_source.on_change('data', calibres_table_source_callback)
+datatable_source.on_change('data', datatable_source_callback)
 
-calibres_table = DataTable(
-    source=calibres_table_source,
+datatable = DataTable(
+    source=datatable_source,
     columns=[
         TableColumn(field='energy', title="Photon Energy, eV", editor=IntEditor()),
         TableColumn(field='peak_pos0', title="Reference Peak Position", editor=IntEditor()),
@@ -137,7 +137,7 @@ calibres_table = DataTable(
 
 
 # THz calibration plot
-calib_thz_plot = Plot(
+thz_scan_plot = Plot(
     title=Title(text="THz calibration"),
     x_range=DataRange1d(),
     y_range=DataRange1d(),
@@ -148,42 +148,41 @@ calib_thz_plot = Plot(
 )
 
 # ---- tools
-calib_thz_plot.add_tools(PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool())
+thz_scan_plot.add_tools(PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool())
 
 # ---- axes
-calib_thz_plot.add_layout(
+thz_scan_plot.add_layout(
     LinearAxis(axis_label='Stage delay position'), place='below')
-calib_thz_plot.add_layout(
+thz_scan_plot.add_layout(
     LinearAxis(axis_label='Energy shift, eV', major_label_orientation='vertical'), place='left')
 
 # ---- grid lines
-calib_thz_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
-calib_thz_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
+thz_scan_plot.add_layout(Grid(dimension=0, ticker=BasicTicker()))
+thz_scan_plot.add_layout(Grid(dimension=1, ticker=BasicTicker()))
 
 # ---- circle glyphs
-calib_thz_point_source = ColumnDataSource(dict(x=[], y=[]))
-thz_fit_c = calib_thz_plot.add_glyph(
-    calib_thz_point_source, Circle(x='x', y='y', line_color='blue'))
+thz_scan_circle_source = ColumnDataSource(dict(x=[], y=[]))
+thz_scan_plot.add_glyph(thz_scan_circle_source, Circle(x='x', y='y', line_color='purple'))
 
 # ---- line glyphs
-calib_thz_fit_source = ColumnDataSource(dict(x=[], y=[]))
-thz_fit_l = calib_thz_plot.add_glyph(calib_thz_fit_source, Line(x='x', y='y', line_color='blue'))
+thz_fit_line_source = ColumnDataSource(dict(x=[], y=[]))
+thz_scan_plot.add_glyph(thz_fit_line_source, Line(x='x', y='y', line_color='purple'))
 
 
 # Calibration folder path text input
-def calib_path_textinput_callback(_attr, _old, _new):
-    update_calib_load_menu()
+def path_textinput_callback(_attr, _old, _new):
+    update_load_dropdown_menu()
 
-calib_path_textinput = TextInput(
+path_textinput = TextInput(
     title="Calibration Folder Path:", value=os.path.join(os.path.expanduser('~')), width=525)
-calib_path_textinput.on_change('value', calib_path_textinput_callback)
+path_textinput.on_change('value', path_textinput_callback)
 
 
 # Calibrate button
 def calibrate_button_callback():
-    palm.calibrate_etof(folder_name=calib_path_textinput.value)
+    palm.calibrate_etof(folder_name=path_textinput.value)
 
-    calibres_table_source.data.update(
+    datatable_source.data.update(
         energy=palm.etofs['0'].calib_data.index.tolist(),
         peak_pos0=palm.etofs['0'].calib_data['calib_tpeak'].tolist(),
         peak_pos1=palm.etofs['1'].calib_data['calib_tpeak'].tolist())
@@ -192,18 +191,18 @@ def update_calibration_plot(calib_res):
     etof_ref = palm.etofs['0']
     etof_str = palm.etofs['1']
 
-    calib_waveform_source0.data.update(
+    waveform_ref_source.data.update(
         xs=len(etof_ref.calib_data)*[list(range(etof_ref.internal_time_bins))],
         ys=etof_ref.calib_data['waveform'].tolist(),
         en=etof_ref.calib_data.index.tolist())
 
-    calib_waveform_source1.data.update(
+    waveform_str_source.data.update(
         xs=len(etof_str.calib_data)*[list(range(etof_str.internal_time_bins))],
         ys=etof_str.calib_data['waveform'].tolist(),
         en=etof_str.calib_data.index.tolist())
 
-    phot_peak_pos_ref.location = etof_ref.calib_t0
-    phot_peak_pos_str.location = etof_str.calib_t0
+    photon_peak_ref_span.location = etof_ref.calib_t0
+    photon_peak_str_span.location = etof_str.calib_t0
 
     def plot_fit(time, calib_a, calib_b):
         time_fit = np.linspace(np.nanmin(time), np.nanmax(time), 100)
@@ -216,8 +215,8 @@ def update_calibration_plot(calib_res):
         circle.data.update(x=x, y=y)
         line.data.update(x=x_fit, y=y_fit)
 
-    update_plot(calib_res['0'], calib_point_source0, calib_fit_source0)
-    update_plot(calib_res['1'], calib_point_source1, calib_fit_source1)
+    update_plot(calib_res['0'], fit_ref_circle_source, fit_ref_line_source)
+    update_plot(calib_res['1'], fit_str_circle_source, fit_str_line_source)
 
     calib_const_div.text = f"""
     a_str = {etof_str.calib_a:.2f}<br>
@@ -233,45 +232,45 @@ calibrate_button.on_click(calibrate_button_callback)
 
 # Save calibration button
 def save_button_callback():
-    palm.save_etof_calib(path=calib_path_textinput.value)
-    update_calib_load_menu()
+    palm.save_etof_calib(path=path_textinput.value)
+    update_load_dropdown_menu()
 
 save_button = Button(label="Save", button_type='default', width=135)
 save_button.on_click(save_button_callback)
 
 
 # Load calibration button
-def load_button_callback(selection):
+def load_dropdown_callback(selection):
     if selection:
-        palm.load_etof_calib(calib_path_textinput.value, selection)
+        palm.load_etof_calib(path_textinput.value, selection)
 
-        calibres_table_source.data.update(
+        datatable_source.data.update(
             energy=palm.etofs['0'].calib_data.index.tolist(),
             peak_pos0=palm.etofs['0'].calib_data['calib_tpeak'].tolist(),
             peak_pos1=palm.etofs['1'].calib_data['calib_tpeak'].tolist())
 
         # Drop selection, so that this callback can be triggered again on the same dropdown menu
         # item from the user perspective
-        load_button.value = ''
+        load_dropdown.value = ''
 
-def update_calib_load_menu():
+def update_load_dropdown_menu():
     new_menu = []
-    if os.path.isdir(calib_path_textinput.value):
-        with os.scandir(calib_path_textinput.value) as it:
+    if os.path.isdir(path_textinput.value):
+        with os.scandir(path_textinput.value) as it:
             for entry in it:
                 if entry.is_file() and entry.name.endswith(('.palm')):
                     new_menu.append((entry.name[:-5], entry.name))
-        load_button.button_type = 'default'
-        load_button.menu = sorted(new_menu, reverse=True)
+        load_dropdown.button_type = 'default'
+        load_dropdown.menu = sorted(new_menu, reverse=True)
     else:
-        load_button.button_type = 'danger'
-        load_button.menu = new_menu
+        load_dropdown.button_type = 'danger'
+        load_dropdown.menu = new_menu
 
-doc.add_next_tick_callback(update_calib_load_menu)
-doc.add_periodic_callback(update_calib_load_menu, 10000)
+doc.add_next_tick_callback(update_load_dropdown_menu)
+doc.add_periodic_callback(update_load_dropdown_menu, 10000)
 
-load_button = Dropdown(label="Load", menu=[], width=135)
-load_button.on_click(load_button_callback)
+load_dropdown = Dropdown(label="Load", menu=[], width=135)
+load_dropdown.on_click(load_dropdown_callback)
 
 
 # Fitting equation
@@ -291,9 +290,9 @@ calib_const_div = Div(
 
 # assemble
 tab_calibration_layout = row(
-    column(calib_wf_plot, calib_fit_plot, calib_thz_plot), Spacer(width=30),
+    column(waveform_plot, fit_plot, thz_scan_plot), Spacer(width=30),
     column(
-        calib_path_textinput, calibrate_button, row(save_button, Spacer(width=10), load_button),
-        calibres_table, fit_eq_div, calib_const_div))
+        path_textinput, calibrate_button, row(save_button, Spacer(width=10), load_dropdown),
+        datatable, fit_eq_div, calib_const_div))
 
 tab_calibration = Panel(child=tab_calibration_layout, title="Calibration")
