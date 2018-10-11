6f4a1f9adb4904f38d959683601a738310d39207
Ivan Usov <ivan.usov@psi.ch>
2018-10-11 11:08:17 +0200

    Rename panel_calib into panel_etof_calib for clarity

--- apps/palm/main.py
+++ apps/palm/main.py
@@ -4,7 +4,7 @@ from bokeh.models import Tabs
 
 import photodiag
 import receiver
-import panel_calib
+import panel_etof_calib
 import panel_thz_calib
 import panel_h5file
 import panel_stream
@@ -18,8 +18,8 @@ palm = photodiag.PalmSetup(
     noise_range=[0, 250], energy_range=np.linspace(4850, 5150, 301))
 
 # Final layout
-tab_calib = panel_calib.create(palm)
+tab_etof_calib = panel_etof_calib.create(palm)
 tab_thz_calib = panel_thz_calib.create(palm)
 tab_h5file = panel_h5file.create(palm)
 tab_stream = panel_stream.create(palm)
-doc.add_root(Tabs(tabs=[tab_calib, tab_thz_calib, tab_h5file, tab_stream]))
+doc.add_root(Tabs(tabs=[tab_etof_calib, tab_thz_calib, tab_h5file, tab_stream]))
