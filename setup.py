4729a3b6c7c3deab5bd12e647ceedb31eef97f3f
Ivan Usov <ivan.usov@psi.ch>
2019-03-01 16:00:34 +0100

    Updating for version 0.2.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="photodiag",
-    version="0.2.8",
+    version="0.2.9",
     author="Ivan Usov",
     author_email="ivan.usov@psi.ch",
     description="Photon diagnostics tools at SwissFel",
