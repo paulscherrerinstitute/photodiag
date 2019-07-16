ad63e206fb253fe94ca004ab202fc073698c4356
Ivan Usov <ivan.usov@psi.ch>
2019-07-16 18:05:39 +0200

    track a version number inside a package

--- photodiag/__init__.py
+++ photodiag/__init__.py
@@ -1,2 +1,4 @@
 from .palm_code import PalmSetup
 from .spatial_encoder import SpatialEncoder
+
+__version__ = "0.3.2"
--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+import re
+
 from setuptools import setup
+
+with open("photodiag/__init__.py") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
     name="photodiag",
-    version="0.3.2",
+    version=version,
     author="Ivan Usov",
     author_email="ivan.usov@psi.ch",
     description="Photon diagnostics tools at SwissFel",
