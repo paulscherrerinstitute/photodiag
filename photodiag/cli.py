d04e1022baeb96de662d883265a4e3869f9fdecd
Ivan Usov <ivan.usov@psi.ch>
2019-07-17 23:16:22 +0200

    remove bokeh serve interface

--- photodiag/cli.py
+++ photodiag/cli.py
@@ -1,28 +1,66 @@
+import argparse
+import logging
+import os
+
+from bokeh.application.application import Application
+from bokeh.application.handlers import DirectoryHandler
+from bokeh.server.server import Server
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 def main():
-    """
-    Execute the "photodiag" command line program.
+    """The photodiag command line interface.
 
-    This is a wrapper around 'bokeh serve' command which provides a user interface to launch
+    This is a wrapper around bokeh server that provides an interface to launch
     applications bundled with the photodiag package.
-
-    For more information, see:
-    https://bokeh.pydata.org/en/latest/docs/reference/command/subcommands/serve.html
     """
+    parser = argparse.ArgumentParser(
+        prog='photodiag', formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    import os
-    import sys
-    import subprocess
+    parser.add_argument(
+        'app',
+        type=str,
+        choices=['palm'],
+        help="photodiag application",
+    )
 
-    apps_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'apps')
+    parser.add_argument(
+        '--port', type=int, default=5006, help="the port to listen on for HTTP requests"
+    )
 
-    # TODO: generalize photodiag parsing after python/3.7 release
-    # due to an issue with 'argparse' (https://bugs.python.org/issue14191),
-    # which is supposed to be fixed in python/3.7, keep parsing unflexible, but very simple
-    _, app_name, *app_args = sys.argv
+    parser.add_argument(
+        '--allow-websocket-origin',
+        metavar='HOST[:PORT]',
+        type=str,
+        action='append',
+        default=None,
+        help="hostname that can connect to the server websocket",
+    )
 
-    command = ['bokeh', 'serve', os.path.join(apps_path, app_name), *app_args]
-    print(' '.join(command))
-    subprocess.run(command)
+    parser.add_argument(
+        '--args',
+        nargs=argparse.REMAINDER,
+        default=[],
+        help="command line arguments for the photodiag application",
+    )
+
+    args = parser.parse_args()
+
+    app_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'apps', args.app)
+    logger.info(app_path)
+
+    handler = DirectoryHandler(filename=app_path, argv=args.args)
+    server = Server(
+        {'/': Application(handler)},
+        port=args.port,
+        allow_websocket_origin=args.allow_websocket_origin,
+    )
+
+    server.start()
+    server.io_loop.start()
 
 
 if __name__ == "__main__":
