3feebe9cfe0f2de8c1c909b0d57667e7f9bd3c0f
Ivan Usov <ivan.usov@psi.ch>
2020-05-26 20:42:36 +0200

    Add savgol filter

--- photodiag/utils.py
+++ photodiag/utils.py
@@ -1,6 +1,7 @@
 import json
 
 import numpy as np
+from scipy import signal
 
 
 def read_eco_scan(filepath):
@@ -27,16 +28,13 @@ def read_eco_scan(filepath):
 
 def find_edge(data, step_length=50, edge_type="falling", refinement=1):
     # refine data
-    def _interp(fp, xp, x):  # utility function to be used with apply_along_axis
-        return np.interp(x, xp, fp)
-
     data_length = data.shape[1]
     refined_data = np.apply_along_axis(
-        _interp,
+        _interpolate_row,
         axis=1,
         arr=data,
-        x=np.arange(0, data_length - 1, refinement),
-        xp=np.arange(data_length),
+        interp_energy=np.arange(0, data_length - 1, refinement),
+        energy=np.arange(data_length),
     )
 
     # prepare a step function and refine it
@@ -59,3 +57,20 @@ def find_edge(data, step_length=50, edge_type="falling", refinement=1):
     edge_position += np.floor(step_length / 2)
 
     return {"edge_pos": edge_position, "xcorr": xcorr, "xcorr_ampl": xcorr_amplitude}
+
+
+def savgol_filter(data, period, window, steps):
+    C = 2.99792458
+    lambda_nm = np.linspace(*window, steps)
+    freq = C / lambda_nm
+    interp_freq = np.linspace(C / window[1], C / window[0], steps)
+
+    tmp = np.apply_along_axis(_interpolate_row, 0, data[::-1], freq[::-1], interp_freq)[::-1]
+    tmp2 = signal.savgol_filter(tmp, period, 1)
+    data_out = np.apply_along_axis(_interpolate_row, 0, tmp2[::-1], interp_freq, freq[::-1])[::-1]
+
+    return data_out
+
+
+def _interpolate_row(data, energy, interp_energy):
+    return np.interp(interp_energy, energy, data)
